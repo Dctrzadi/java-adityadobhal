085589545a70b53edfd9b4f30b115fdb981d3cc7
ADITYA DOBHAL <123196030+Dctrzadi@users.noreply.github.com>
2026-04-16 19:06:48 +0530

    Create practice.java

---
+++ practice.java
@@ -0,0 +1,15 @@
+public class practice {
+    public static void main(String[] args) {
+        String name = "Hello World";
+        String name2 ="Java";
+        System.out.println(name);
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.charAt(0));
+        System.out.println(name.length());
+        System.out.println(name.contains("World"));
+        System.out.println(name.concat("!"));
+        System.out.println(name.compareTo(name2));
+        System.out.println(name.equals(name2));
+    }
+}
